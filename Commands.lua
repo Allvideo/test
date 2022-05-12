f86b5c1ccb5d0fdb839b7d6e45d714341b2482f3
vq2unys9r8sv7ad5 <86698043+vq2unys9r8sv7ad5@users.noreply.github.com>
2022-05-11 22:34:06 -0600

    Create Commands.lua

---
+++ Commands.lua
@@ -0,0 +1,10 @@
+return {
+    Benx = "Benx the user.",
+    Bring = "Brings the user to you.",
+    Fling = "Flings the user.",
+    Freeze = "Freezes the user.",
+    Kill = "Kills the user.",
+    Say = "Makes the user say whatever.",
+    Unbenx = "Unbenx the user.",
+    Unfreeze = "Unfreezes the user."
+}
