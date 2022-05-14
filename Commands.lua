3671f22af5b9e13d085620a7edb90f0033a632d0
vq2unys9r8sv7ad5 <86698043+vq2unys9r8sv7ad5@users.noreply.github.com>
2022-05-14 13:19:16 -0600

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,10 +1,14 @@
 return {
+    Ban = "Fake bans the user.",
     Benx = "Benx the user.",
     Bring = "Brings the user to you.",
+    Crash = "Crashes the user.",
     Fling = "Flings the user.",
     Freeze = "Freezes the user.",
+    Kick = "Kicks the user with reason.",
     Kill = "Kills the user.",
-    Say = "Makes the user say whatever.",
+    Say = "Makes the user say message.",
+    Shutdown = "Shuts the user's game down.",
     Unbenx = "Unbenx the user.",
     Unfreeze = "Unfreezes the user."
 }
